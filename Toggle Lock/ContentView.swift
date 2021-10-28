725077fdef0b9c987d3eed63d2f7635380a16631
mariusaee <mariusae@me.com>
2021-10-28 11:56:51 +0300

    Initial Commit

---
+++ Toggle Lock/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Toggle Lock
+//
+//  Created by Marius Malyshev on 28.10.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Toggle Lock/Toggle_LockApp.swift
@@ -0,0 +1,17 @@
+//
+//  Toggle_LockApp.swift
+//  Toggle Lock
+//
+//  Created by Marius Malyshev on 28.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct Toggle_LockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
