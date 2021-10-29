23b135a8044f29e5bf6c863f646ce16dcbeafaf5
mariusaee <mariusae@me.com>
2021-10-29 13:25:57 +0300

    ContentView to StarterView

--- Toggle Lock/Toggle_LockApp.swift
+++ Toggle Lock/Toggle_LockApp.swift
@@ -14,7 +14,7 @@ struct Toggle_LockApp: App {
     
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            StarterView()
                 .environmentObject(toggleLockViewModel)
                 .onChange(of: scenePhase) { newValue in
                     switch newValue {
