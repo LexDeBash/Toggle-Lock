db62c0c0c9cbf2948e5ede270076ce93bd79077c
mariusaee <mariusae@me.com>
2021-11-02 12:03:55 +0300

    blur

--- Toggle Lock/Toggle_LockApp.swift
+++ Toggle Lock/Toggle_LockApp.swift
@@ -12,18 +12,22 @@ struct Toggle_LockApp: App {
     @StateObject var toggleLockViewModel = ToggleLockViewModel()
     @Environment(\.scenePhase) var scenePhase
     
+    @State var blurRadius: CGFloat = 0
+    
     var body: some Scene {
         WindowGroup {
             StarterView()
                 .environmentObject(toggleLockViewModel)
+                .blur(radius: blurRadius)
                 .onChange(of: scenePhase) { sceneMode in
                     switch sceneMode {
                     case .background:
                         toggleLockViewModel.isUnlocked = false
+                        print("Статус изменился в бэке")
                     case .inactive:
-                        print("inactive")
+                        blurRadius = 5
                     case .active:
-                        print("active")
+                        blurRadius = 0
                     @unknown default:
                         print("unknown")
                     }
